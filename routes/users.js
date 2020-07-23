166a7b918b51851cb302513fe237bf364b5b8783
sserges <agounkeserge@gmail.com>
2020-07-23 21:45:36 +0000

    DELETE users/:id done

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get('/', (req, res) => {
   res.send(users);
@@ -20,6 +20,12 @@ router.get('/:id', (req, res) => {
   const { id } = req.params;
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with the id ${id} deleted from the database.`);
 });
 
 export default router;
